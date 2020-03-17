9ac2a7c28c49eb7c1c7fa160712b8a958ce97fd0
devgitusr <mmhossen@protonmail.com>
2020-03-17 14:32:20 +0100

    v0.0.2

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `<h1 style="color:blue;">V0.0.1</h1>`)
+		fmt.Fprintf(w, `<h1 style="color:green;">V0.0.2</h1>`)
 	})
 	log.Printf("Serving on HTTP port 80")
 	http.ListenAndServe(":80", nil)
